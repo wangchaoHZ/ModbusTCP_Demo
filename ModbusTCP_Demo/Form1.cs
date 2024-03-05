2d82548c52535806ee17a0e30dc83a24f93681f5
wangchaoHZ <wangchaohzx@gmail.com>
2024-03-05 16:07:50 +0800

    add connect demo

--- ModbusTCP_Demo/Form1.cs
+++ ModbusTCP_Demo/Form1.cs
@@ -8,6 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using HslCommunication.Profinet;
+using HslCommunication;
+using HslCommunication.ModBus;
+using System.Threading;
+using static System.Collections.Specialized.BitVector32;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace ModbusTCP_Demo
 {
     public partial class Form1 : Form
@@ -15,6 +30,35 @@ namespace ModbusTCP_Demo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private ModBusTcpClient busTcpClient = null;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            busTcpClient?.ConnectClose();
+            busTcpClient = new ModBusTcpClient("127.0.0.1", 502, 1);
+
+            try
+            {
+                OperateResult connect = busTcpClient.ConnectServer();
+                if (connect.IsSuccess)
+                {
+                    MessageBox.Show("连接成功！");
+                }
+                else
+                {
+                    MessageBox.Show("连接失败！");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
