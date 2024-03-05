8da72c73cb785465a89385ebf3789bd5db661212
wangchaoHZ <wangchaohzx@gmail.com>
2024-03-05 16:09:58 +0800

    fix connect demo

--- ModbusTCP_Demo/Form1.cs
+++ ModbusTCP_Demo/Form1.cs
@@ -1,27 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using HslCommunication.Profinet;
 using HslCommunication;
 using HslCommunication.ModBus;
-using System.Threading;
-using static System.Collections.Specialized.BitVector32;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ModbusTCP_Demo
 {
